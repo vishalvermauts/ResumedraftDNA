8c10ed6cd926e96832e1cef3d8d99bcb87200003
Vishal Verma <vishalvr28@gmail.com>
2026-08-06 02:21:28 +0530

    feat: add australian greenhouse/lever tech companies to seed script
    Includes Culture Amp, Eucalyptus, HubSpot, Asana, Zip, Bugcrowd, Buildkite, Klaviyo, etc.

--- seed_companies.py
+++ seed_companies.py
@@ -161,6 +161,228 @@ SEED_COMPANIES = [
             "priority": ["ashby", "jsonld", "ai_search"],
             "configuration": {}
         }
+    },
+    
+    # Newly Added Greenhouse & Lever companies from Australia job listing
+    {
+        "companyName": "JCDecaux",
+        "careersUrl": "https://boards.greenhouse.io/jcdecaux",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "jcdecaux",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Eucalyptus",
+        "careersUrl": "https://boards.greenhouse.io/eucalyptus",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "eucalyptus",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Culture Amp",
+        "careersUrl": "https://boards.greenhouse.io/cultureamp",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "cultureamp",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "HubSpot",
+        "careersUrl": "https://boards.greenhouse.io/hubspot",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "hubspot",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Asana",
+        "careersUrl": "https://boards.greenhouse.io/asana",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "asana",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Navan",
+        "careersUrl": "https://boards.greenhouse.io/navan",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "navan",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "AirTrunk",
+        "careersUrl": "https://boards.greenhouse.io/airtrunk",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "airtrunk",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Sonder.io",
+        "careersUrl": "https://boards.greenhouse.io/sonder",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "sonder",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Bugcrowd",
+        "careersUrl": "https://boards.greenhouse.io/bugcrowd",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "bugcrowd",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "LogicMonitor",
+        "careersUrl": "https://boards.greenhouse.io/logicmonitor",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "logicmonitor",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Postman",
+        "careersUrl": "https://boards.greenhouse.io/postman",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "postman",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Recorded Future",
+        "careersUrl": "https://boards.greenhouse.io/recordedfuture",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "recordedfuture",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Adyen",
+        "careersUrl": "https://boards.greenhouse.io/adyen",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "adyen",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Buildkite",
+        "careersUrl": "https://boards.greenhouse.io/buildkite",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "buildkite",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Workato",
+        "careersUrl": "https://boards.greenhouse.io/workato",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "workato",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Klaviyo",
+        "careersUrl": "https://boards.greenhouse.io/klaviyo",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "klaviyo",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Cribl",
+        "careersUrl": "https://boards.greenhouse.io/cribl",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "cribl",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Netskope",
+        "careersUrl": "https://boards.greenhouse.io/netskope",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "netskope",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "FeverUp",
+        "careersUrl": "https://boards.greenhouse.io/feverup",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "feverup",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Roller",
+        "careersUrl": "https://boards.greenhouse.io/roller",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "roller",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "New Relic",
+        "careersUrl": "https://boards.greenhouse.io/newrelic",
+        "connector": {
+            "type": "greenhouse",
+            "boardToken": "newrelic",
+            "priority": ["greenhouse", "jsonld", "ai_search"],
+            "configuration": {}
+        }
+    },
+    {
+        "companyName": "Remote",
+        "careersUrl": "https://jobs.lever.co/remote",
+        "connector": {
+            "type": "lever",
+            "boardToken": "remote",
+            "priority": ["lever", "jsonld", "ai_search"],
+            "configuration": {}
+        }
     }
 ]
 
